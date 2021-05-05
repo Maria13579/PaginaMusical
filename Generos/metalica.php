b750c01fcac46731856963b1e31905280418c02c
Maria13579 <patymsr3@gmail.com>
2021-05-05 12:40:35 -0600

    d

--- Generos/metalica.php
+++ Generos/metalica.php
@@ -3,10 +3,12 @@
     <li>
       <div class="collapsible-header"><i class="material-icons"></i>Metalica</div>
       <div class="collapsible-body"><span>
-      <img src="Recursos/r1.png"/> </span></div>
-
+      <img src="Recursos/r1.png"/>
+      
+      
+      
+       </span></div>
     </li>
-    <img src="Recursos/r1.png"/>
     <li>
       <div class="collapsible-header"><i class="material-icons"></i>The beatles</div>
       <div class="collapsible-body"><span>
