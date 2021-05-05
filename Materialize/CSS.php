a4bc1acfe69b1f6d31d513cd198296399427938d
Maria13579 <patymsr3@gmail.com>
2021-05-04 19:58:06 -0600

    b

--- Materialize/CSS.php
+++ Materialize/CSS.php
@@ -14,12 +14,12 @@
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Generos Musicales</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="metalica.php">ROCK</a></li>
-        <li><a href="urbano.php">URBANO</a></li>
-        <li><a href="pop.php">POP</a></li>
-        <li><a href="electronica.php">ELECTRONICA</a></li>
-        <li><a href="salsa.php">SALSA</a></li>
-        <li><a href="bachata.php">BACHATA</a></li>
+        <li><a href="Generos/metalica.php">ROCK</a></li>
+        <li><a href="Generos/urbano.php">URBANO</a></li>
+        <li><a href="Generos/pop.php">POP</a></li>
+        <li><a href="Generos/electronica.php">ELECTRONICA</a></li>
+        <li><a href="Generos/salsa.php">SALSA</a></li>
+        <li><a href="Generos/bachata.php">BACHATA</a></li>
       </ul>
     </div>
   </nav>
