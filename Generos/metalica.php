c28de718188371be3f141f0ef028343e3c26038e
Maria13579 <patymsr3@gmail.com>
2021-05-07 07:51:35 -0600

    E

--- Generos/metalica.php
+++ Generos/metalica.php
@@ -3,7 +3,7 @@
     <li>
       <div class="collapsible-header"><i class="material-icons"></i>Metalica</div>
       <div class="collapsible-body"><span>
-      <img src="Recursos/r1.png"/>
+      <img src="RECURSOS/r1.png"/>
       
       
       
@@ -12,25 +12,25 @@
     <li>
       <div class="collapsible-header"><i class="material-icons"></i>The beatles</div>
       <div class="collapsible-body"><span>
-      <img src="Recursos/r2.png"/> 
+      <img src="RECURSOS/r2.png"/> 
       </span></div>
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons"></i>Twenty One Pilots  </div>
       <div class="collapsible-body"><span>
-      <img src="Recursos/r3.png"/> 
+      <img src="RECURSOS/r3.png"/> 
       </span></div>
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons"></i>Pink Floyd</div>
       <div class="collapsible-body"><span>
-      <img src="Recursos/r4.png"/> 
+      <img src="RECURSOS/r4.png"/> 
       </span></div>
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons"></i>The Rolling Stone  </div>
       <div class="collapsible-body"><span>
-      <img src="Recursos/r5.png"/> 
+      <img src="RECURSOS/r5.png"/> 
       </span></div>
     </li> 
   </ul>
