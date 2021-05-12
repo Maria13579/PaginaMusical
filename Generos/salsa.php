86bcd81649a33320c947dc7e90f9889db22d6c51
Maria13579 <patymsr3@gmail.com>
2021-05-11 18:07:34 -0600

    p

--- Generos/salsa.php
+++ Generos/salsa.php
@@ -1,4 +1,13 @@
-
+<html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="../Framework/Materialize/css/materialize.min.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
     <nav> 
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Generos Musicales</a>
@@ -258,4 +267,8 @@
       </span></div>
     </li> 
   </ul>
+     <!--JavaScript at end of body for optimized loading-->
+     <script type="text/javascript" src="../Framework/Materialize/js/materialize.min.js"></script>
+    </body>
+</html>
   
